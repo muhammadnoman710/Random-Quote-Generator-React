439868fa1f820a347186c0b36d5a62d50e82005b
muhammadnoman710 <nomi32655@gmail.com>
2022-12-16 20:41:52 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      advice: ''
+    }
+  }
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        // following line can also be written in a destructuring way like
+        //console.log(response.data.slip.advice)
+        const { advice } = response.data.slip
+        //the following line can be written also as
+        //this.setState({advice : advice})
+        this.setState({ advice })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  componentWillUnmount() {
+    console.log("unmounting", this.fetchAdvice)
+  }
+  render() {
+    const { advice } = this.state;
+    //OR in return it can be written as this.state.advice
+    return (
+      <div className='app'>
+        <div className='card'>
+          <h1 className='heading'>{advice}</h1>
+          <button className='button' onClick={() => { this.fetchAdvice() }}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
